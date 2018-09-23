8dd1a47765d7a1171a2e6ee46e0051f8539f77cf
Maxberman <mb3ax@virginia.edu>
2018-09-23 14:29:47 -0400

    added contact us and whatisthis pages, changed index a bit, got nav links working, started on email handling

--- festivalpickr/urls.py
+++ festivalpickr/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-path("",views.index,name='index'),
+path("index.html",views.index,name='index'),
 path("getspotify",views.getspotify,name='getspotify'),
 path("landing/",views.landing,name='landing'),
-path("refreshlanding",views.refreshlanding,name='refreshlanding')
+path("refreshlanding",views.refreshlanding,name='refreshlanding'),
+path("whatisthis.html",views.whatisthis,name='whatisthis'),
+path("contactus.html",views.contactus,name='contactus'),
+path("toemail",views.toemail,name='toemail'),
 ]
