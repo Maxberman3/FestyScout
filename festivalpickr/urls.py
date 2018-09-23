5952b8b405d598aa545142dd09bc5c7df3476585
Maxberman <mb3ax@virginia.edu>
2018-09-23 15:03:35 -0400

    fixed bug

--- festivalpickr/urls.py
+++ festivalpickr/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
 path("",views.index,name='index'),
-path("/index.html",views.index,name='index'),
+path("index.html",views.index,name='index'),
 path("getspotify",views.getspotify,name='getspotify'),
 path("landing/",views.landing,name='landing'),
 path("refreshlanding",views.refreshlanding,name='refreshlanding'),
