0075a5b674ee0e13ab6727a74f385469ddec9e7f
Maxberman <mb3ax@virginia.edu>
2018-10-30 11:50:58 -0400

    still trying to deploy 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecomproject',
-        'USER': 'ecomprojectuser',
-        'PASSWORD': PG_PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'ecomproject',
+        #'USER': 'ecomprojectuser',
+        #'PASSWORD': PG_PASSWORD,
+        #'HOST': 'localhost',
+        #'PORT': '',
+    #}
 }
 
 
