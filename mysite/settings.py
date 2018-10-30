a91953c4ebd2b353df10535a6d6f5d15c27be1e2
Maxberman <mb3ax@virginia.edu>
2018-10-29 23:28:26 -0400

    set up caching of static assets

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, "static")
 #]
